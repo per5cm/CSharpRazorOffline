cc7fb3793754aa431ac597dd1422d2289540dd5b
per5cm <per5cm@proton.me>
2025-12-16 14:59:48 +0100

    bump

---
+++ Blazor_W_Copy/VPets/VPets/VPets/Models/Pet.cs
@@ -0,0 +1,11 @@
+﻿namespace VPets.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Species { get; set; } = string.Empty;
+        public int Age { get; set; }
+    }
+}
+
