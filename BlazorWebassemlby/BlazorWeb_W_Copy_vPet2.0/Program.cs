451fbf09773c963c5dc5966d8c78546a014ac65e
per5cm <per5cm@proton.me>
2026-01-31 23:31:15 +0100

    first commit

---
+++ BlazorWebassemlby/BlazorWeb_W_Copy_vPet2.0/Program.cs
@@ -0,0 +1,11 @@
+using BlazorWeb_W_Copy_vPet2._0;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
---
+++ BlazorWebassemlby/BlazorWeb_W_Copy_vPet2.0/tailwind.config.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+    content: [
+        "./Pages/**/*.razor",
+        "./Layout/**/*.razor",
+        "./**/*.razor",
+        "./wwwroot/**/*.html"
+    ],
+    theme: { extend: {} },
+    plugins: []
+}
